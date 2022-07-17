1c961a59badc4a5d312dd703f4d2ad0e259c3cc3
ElenaMalysheva <photo.malysheva@gmail.com>
2022-07-17 22:16:25 +0400

    example5

---
+++ Example005_if/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username =  Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура!");
+
+}
+else
+{
+    Console.WriteLine(" не Ура( ");
+}
