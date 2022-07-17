e582b5ad95174bbae039b0b7830eb99584b66e5c
ElenaMalysheva <photo.malysheva@gmail.com>
2022-07-17 21:59:49 +0400

    after weekwnd

--- Examplie003_sum/Program.cs
+++ Examplie003_sum/Program.cs
@@ -1,3 +1,6 @@
-﻿int numberA = 3;
-int numberB = 4;
+﻿//int numberA = 3;
+int numberA = new Random().Next(1,10); //1 2 3 4...9
+
+//int numberB = 4;
+int numberB = new Random().Next(1,10);
 Console.WriteLine(numberA + numberB);
