49cb2950052c93c6f4bf0c3d3e321307ed6ab5f7
ElenaMalysheva <photo.malysheva@gmail.com>
2022-07-19 18:12:20 +0400

    example09

---
+++ Example 009_array/Program.cs
@@ -0,0 +1,21 @@
+﻿
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+//вводим массив
+int[] array = {11,12,13,43,42,41,53,53,54};
+
+
+
+
+int max = Max(
+    Max(array[0], array[1], array[2]), 
+    Max(array[3], array[4], array[5]),  
+    Max(array[6], array[7], array[8]));
+
+
+Console.WriteLine(max); 
