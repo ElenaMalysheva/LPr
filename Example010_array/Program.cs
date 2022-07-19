1d086bb792ff8a447394ccacc5d6d0b27b901572
ElenaMalysheva <photo.malysheva@gmail.com>
2022-07-19 18:23:55 +0400

    example10

---
+++ Example010_array/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = {1,12,13,14,51,25,24,23,54,53,44};
+
+int n = array.Length; //количество элементов в массиве
+int find = 51;
+int index = 0;
+
+while(index<n){
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break; //прерываем цикл
+    }
+    index++;
+}
+
